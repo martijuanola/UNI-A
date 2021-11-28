835038f96f4949413d8d1b3ad1cdb0811542e733
marti.juanola <marti.juanola@est.fib.upc.edu>
2021-11-28 16:52:10 +0100

    Canvis a carpeta entrega!!!
    Tothom ha de posar el seu fitxer i els aclariments que calguin al README
    Recordar canviar els include dels .h a cada fitxer que sino no ho trobarà

--- local_search.sh
+++ local_search.sh
@@ -1,32 +1,27 @@
 EXE_FILE=./bin/local_search
 #INPUT_FILE=data/graph_jazz.txt
-OUTPUT_FILE=src/local_search/resultatsH.txt
+OUTPUT_FILE=src/local_search/resultatsLS.txt
 
 N=5
 
-OPERADORS=3
+HEURISTIC=3
+ALGORISME=2
 SI=2
 
-DEPTH=7
+#s'ha de canviar després de l'experiment
+T=10000
+ITR=15
+K=7
+L=0.1
 
 make local_search
 
 echo -e "A\tH\tMIN\tAVG\tVSD\tTIME\tTSD"> $OUTPUT_FILE
 
-#data/soc-gplus.txt data/graph_actors_dat.txt
 for INPUT_FILE in data/soc-gplus.txt data/graph_CA-CondMat.txt data/graph_CA-AstroPh.txt data/graph_CA-HepPh.txt data/graph_actors_dat.txt data/ego-facebook.txt data/socfb-Brandeis99.txt data/socfb-Mich67.txt data/graph_jazz.txt data/graph_football.txt
 do
 	echo -e $INPUT_FILE >> $OUTPUT_FILE
-	for ALGORISME in 2
-	do
-		for HEURISTIC in 1 2 3
-		do
-			echo -n -e $ALGORISME "\t" >> $OUTPUT_FILE
-			echo -n -e $HEURISTIC "\t" >> $OUTPUT_FILE
-			$EXE_FILE -i $INPUT_FILE -n $N -p	-a $ALGORISME -o $OPERADORS -si $SI -h $HEURISTIC -d $DEPTH >> $OUTPUT_FILE
-		done
-		echo "" >> $OUTPUT_FILE
-	done
+	$EXE_FILE -i $INPUT_FILE -n $N -p	-a $ALGORISME -o $OPERADORS -si $SI -h $HEURISTIC -d $DEPTH >> $OUTPUT_FILE
 	echo -e "\n" >> $OUTPUT_FILE
 done
 echo "END" >> $OUTPUT_FILE
